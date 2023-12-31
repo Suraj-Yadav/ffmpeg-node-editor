78ce916feda03bab698761c5929b4f492b77dfac
Suraj Yadav <surajyadav20@outlook.com>
2023-12-30 21:24:48 -0700

    simplified port

--- overlays/imgui/portfile.cmake
+++ overlays/imgui/portfile.cmake
@@ -1,60 +1,25 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-if ("docking-experimental" IN_LIST FEATURES)
-    vcpkg_from_github(
-       OUT_SOURCE_PATH SOURCE_PATH
-       REPO ocornut/imgui
-       REF "v${VERSION}-docking"
-       SHA512 50953097ff809bca0e1cee55268eaa8dad001cf8fae85ae7623f484752d16bdf8e63c04227786361f27e73f7aeda5a8d1f9b0cee83c2aca108ce2712a21b74d9
-       HEAD_REF docking
-       )
-else()
-    vcpkg_from_github(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO thedmd/imgui
     REF 4981fef6649c1c9204f39641fff7d06cc2b1acfe
     SHA512 c445bd6f0d1fcc58627aeb7f7e785292cb89d0a319be4f2295c83453c60a5f5427cb13c9143beb8a90d4f24cf60999ba8fe84529981b0dbbf4172a35db2faa8d
     HEAD_REF layouts
-    )
-endif()
+)
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/imgui-config.cmake.in" DESTINATION "${SOURCE_PATH}")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES 
-    allegro5-binding            IMGUI_BUILD_ALLEGRO5_BINDING
-    dx9-binding                 IMGUI_BUILD_DX9_BINDING
-    dx10-binding                IMGUI_BUILD_DX10_BINDING
-    dx11-binding                IMGUI_BUILD_DX11_BINDING
     dx12-binding                IMGUI_BUILD_DX12_BINDING
     glfw-binding                IMGUI_BUILD_GLFW_BINDING
-    glut-binding                IMGUI_BUILD_GLUT_BINDING
     metal-binding               IMGUI_BUILD_METAL_BINDING
-    opengl2-binding             IMGUI_BUILD_OPENGL2_BINDING
     opengl3-binding             IMGUI_BUILD_OPENGL3_BINDING
     osx-binding                 IMGUI_BUILD_OSX_BINDING
-    sdl2-binding                IMGUI_BUILD_SDL2_BINDING
-    sdl2-renderer-binding       IMGUI_BUILD_SDL2_RENDERER_BINDING
-    vulkan-binding              IMGUI_BUILD_VULKAN_BINDING
     win32-binding               IMGUI_BUILD_WIN32_BINDING
-    freetype                    IMGUI_FREETYPE
-    freetype-lunasvg            IMGUI_FREETYPE_LUNASVG
-    wchar32                     IMGUI_USE_WCHAR32
 )
-
-if ("libigl-imgui" IN_LIST FEATURES)
-    vcpkg_download_distfile(
-        IMGUI_FONTS_DROID_SANS_H
-        URLS
-            https://raw.githubusercontent.com/libigl/libigl-imgui/c3efb9b62780f55f9bba34561f79a3087e057fc0/imgui_fonts_droid_sans.h
-        FILENAME "imgui_fonts_droid_sans.h"
-        SHA512
-            abe9250c9a5989e0a3f2285bbcc83696ff8e38c1f5657c358e6fe616ff792d3c6e5ff2fa23c2eeae7d7b307392e0dc798a95d14f6d10f8e9bfbd7768d36d8b31
-    )
-
-    file(INSTALL "${IMGUI_FONTS_DROID_SANS_H}" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -65,16 +30,6 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
-if ("freetype" IN_LIST FEATURES)
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/imconfig.h" "//#define IMGUI_ENABLE_FREETYPE" "#define IMGUI_ENABLE_FREETYPE")
-endif()
-if ("freetype-lunasvg" IN_LIST FEATURES)
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/imconfig.h" "//#define IMGUI_ENABLE_FREETYPE_LUNASVG" "#define IMGUI_ENABLE_FREETYPE_LUNASVG")
-endif()
-if ("wchar32" IN_LIST FEATURES)
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/imconfig.h" "//#define IMGUI_USE_WCHAR32" "#define IMGUI_USE_WCHAR32")
-endif()
 
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup()
